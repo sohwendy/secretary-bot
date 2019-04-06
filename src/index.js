8ab47585c5da6e1f1c3a08dc6e246d94c3a86a06
wendy <wencodes@gmail.com>
2019-04-06 19:40:52 +0800

    Bug Fix for date (#39)
    - Generate date on the fly
    - Set chatfile to agent instead of using global chatfile

--- src/index.js
+++ src/index.js
@@ -4,24 +4,23 @@ const TelegramBot = require('node-telegram-bot-api');
 const constants = require('../config/constants.js');
 const JsonFileHelper = require('./lib/json-file-helper');
 const Logger = require('./lib/log-helper');
-const StockReporter = require('./worker/stock-report-worker');
-const ForexReporter = require('./worker/forex-report-worker');
-const ReminderReporter = require('./worker/reminder-report-worker');
-const StockMonitor = require('./worker/stock-monitor-worker');
-const ForexMonitor = require('./worker/forex-monitor-worker');
-const ReminderMonitor = require('./worker/reminder-monitor-worker');
-const BankReporter = require('./worker/bank-forex-report-worker');
-const LearnReporter = require('./worker/learn-report-worker');
+const StockRpt = require('./worker/stock-report-worker');
+const ForexRpt = require('./worker/forex-report-worker');
+const ReminderRpt = require('./worker/reminder-report-worker');
+const StockMon = require('./worker/stock-monitor-worker');
+const ForexMon = require('./worker/forex-monitor-worker');
+const ReminderMon = require('./worker/reminder-monitor-worker');
+const BankRpt = require('./worker/bank-forex-report-worker');
+const LearnRpt = require('./worker/learn-report-worker');
 const Agent = require('./agent/agent');
 const BankForexAgent = require('./agent/bank-forex-agent');
-const ReminderMonitorAgent = require('./agent/reminder-monitor-agent');
+const ReminderMonAgent = require('./agent/reminder-monitor-agent');
 
 
 global.debug = true;
 const state = process.argv[2] || '';
-let chatFile = 'chat.json';
 
-const send = async(data) => {
+const send = async(data, chatFile) => {
   try {
     const chat = await JsonFileHelper.read(constants.secretPath(chatFile));
     Logger.log(chatFile, chat);
@@ -36,16 +35,25 @@ const send = async(data) => {
   }
 };
 
-const today = moment().local().startOf('day');
-const day = today.clone().dayOfYear();
-let dates = [];
-for (let i = 0; i < 3; i++) {
-  const d = today.clone().add(i, 'day').format('DD MMM YYYY');
-  dates.push(d);
-}
-let time = moment().local().format('HH');
+const getDates = () => {
+  const today = moment().local().startOf('day');
+  let dates = [];
+  for (let i = 0; i < 3; i++) {
+    const d = today.clone().add(i, 'day').format('DD MMM YYYY');
+    dates.push(d);
+  }
+  return dates;
+};
 
-Logger.log(dates[0], time);
+const getDay = () => {
+  const today = moment().local().startOf('day');
+  return today.clone().dayOfYear();
+};
+
+const getTime = () => {
+  return moment().local().format('HH');
+};
+
 Logger.log('Run State?', state);
 Logger.log(`Enable Reminder? ${process.env.REMINDER || 'No'}`);
 Logger.log(`Enable Stock? ${process.env.STOCK || 'No'}`);
@@ -54,109 +62,116 @@ Logger.log(`Enable Bank? ${process.env.BANK || 'No'}`);
 Logger.log(`Enable Bank? ${process.env.LEARN || 'No'}`);
 
 
-const reminderReporter =  () => Agent.fetch(ReminderReporter, 'reminder', dates).then(send);
-const reminderMonitor =   () => ReminderMonitorAgent.fetch(ReminderMonitor, 'reminder', dates[0], time).then(send);
-const forexReporter =     () => Agent.fetch(ForexReporter, 'forex').then(send);
-const forexMonitor =      () => Agent.fetch(ForexMonitor, 'forex').then(send);
-const stockReporter =     () => Agent.fetch(StockReporter, 'stock').then(send);
-const stockMonitor =      () => Agent.fetch(StockMonitor, 'stock').then(send);
-const bankReporter =      () => BankForexAgent.update(BankReporter, 'bankforex');
-const learnReporter =      () => Agent.fetch(LearnReporter, 'learn', day).then(send);
+const reminderRpt = () => Agent
+  .fetch(ReminderRpt, 'reminder', getDates()).then(data => send(data, 'reminderchat.json'));
+const reminderMon = () => ReminderMonAgent
+  .fetch(ReminderMon, 'reminder', getDates()[0], getTime()).then(data => send(data, 'reminderchat.json'));
+
+const forexRpt = () => Agent
+  .fetch(ForexRpt, 'forex').then(data => send(data, 'forexchat.json'));
+const forexMon = () => Agent
+  .fetch(ForexMon, 'forex').then(data => send(data, 'forexchat.json'));
+
+const stockRpt = () => Agent
+  .fetch(StockRpt, 'stock').then(data => send(data, 'stockchat.json'));
+const stockMon = () => Agent
+  .fetch(StockMon, 'stock').then(data => send(data, 'stockchat.json'));
+
+const learnRpt = () => Agent
+  .fetch(LearnRpt, 'learn', getDay()).then(data => send(data, 'learnchat.json'));
+
+const bankRpt = () => BankForexAgent.update(BankRpt, 'bankforex');
 
 
 if (!state) {
   Logger.log('Fire once...');
-  reminderReporter();
-  reminderMonitor();
-  forexReporter();
-  forexMonitor();
-  stockReporter();
-  // stockMonitor();
-  bankReporter();
-  learnReporter();
+  reminderRpt();
+  reminderMon();
+  forexRpt();
+  forexMon();
+  stockRpt();
+  // stockMon();
+  bankRpt();
+  learnRpt();
 } else {
   Logger.log('Create Cron...');
 
   if (process.env.LEARN == 1) {
-    chatFile = 'learnchat.json';
-    learnReporter();
+    learnRpt();
     Logger.log('learn starts');
     new cron.CronJob({
       cronTime: constants.schedule[state].learn.report,
-      onTick: learnReporter,
+      onTick: learnRpt,
       start: true
     });
   }
 
   if (process.env.REMINDER == 1) {
-    reminderReporter();
-    reminderMonitor();
-    chatFile = 'reminderchat.json';
-    Logger.log('reminder starts', chatFile);
+    reminderRpt();
+    reminderMon();
+    Logger.log('reminder starts');
     new cron.CronJob({
       cronTime: constants.schedule[state].reminder.report,
-      onTick: reminderReporter,
+      onTick: reminderRpt,
       start: true
     });
 
     new cron.CronJob({
       cronTime: constants.schedule[state].reminder.monitor,
-      onTick: reminderMonitor,
+      onTick: reminderMon,
       start: true
     });
   }
 
   if (process.env.FOREX == 1) {
-    forexReporter();
-    forexMonitor();
-    chatFile = 'forexchat.json';
-    Logger.log('forex starts', chatFile);
+    forexRpt();
+    forexMon();
+    Logger.log('forex starts');
     new cron.CronJob({
       cronTime: constants.schedule[state].forex.report,
-      onTick: forexReporter,
+      onTick: forexRpt,
       start: true
     });
 
     new cron.CronJob({
       cronTime: constants.schedule[state].forex.monitor,
-      onTick: forexMonitor,
+      onTick: forexMon,
       start: true
     });
   }
 
   if (process.env.STOCK == 1) {
-    stockReporter();
-    // stockMonitor();
-    chatFile = 'stockchat.json';
-    Logger.log('stock starts', chatFile);
+    stockRpt();
+    // stockMon();
+    Logger.log('stock starts');
     new cron.CronJob({
       cronTime: constants.schedule[state].stock.report,
-      onTick: stockReporter,
+      onTick: stockRpt,
       start: true
     });
 
     new cron.CronJob({
       cronTime: constants.schedule[state].stock.monitor,
-      onTick: stockMonitor,
+      onTick: stockMon,
       start: true
     });
   }
 
   if (process.env.BANK == 1) {
-    bankReporter();
+    bankRpt();
     Logger.log('bank starts');
     new cron.CronJob({
       cronTime: constants.schedule[state].bank.report,
-      onTick: bankReporter,
+      onTick: bankRpt,
       start: true
     });
   }
 
-  new cron.CronJob({
-    cronTime: '00 00 */2 * * *',
-    onTick: () => {
-      Logger.log('heartbeat');
-    },
-    start: true
-  });
+  // new cron.CronJob({
+  //   cronTime: '00 00 */2 * * *',
+  //   onTick: () => {
+  //     Logger.log('heartbeat');
+  //   },
+  //   start: true
+  // });
 }
